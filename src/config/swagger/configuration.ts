bb4202596fbd0df89547e799134e934c969e9978
Juan Sebastián Díaz Osorio <jsdiazo@eafit.edu.co>
2022-03-26 21:48:13 -0500

    swagger implemented

---
+++ src/config/swagger/configuration.ts
@@ -0,0 +1,5 @@
+import { CardList } from 'src/models/card-list/entities/card-list.entity';
+
+export const swaggerConfig = {
+  extraModels: [CardList],
+};
--- src/models/card-list/entities/card-list.entity.ts
+++ src/models/card-list/entities/card-list.entity.ts
@@ -5,18 +5,38 @@ export class CardList {
   @PrimaryGeneratedColumn()
   id: number;
 
+  /**
+   * Credit card type
+   * @example 'MASTERCARD', 'PRIVADA'
+   */
   @Column({ type: 'varchar' })
   type: string;
 
+  /**
+   * Credit card last digits
+   * @example '1234'
+   */
   @Column({ type: 'char', length: 4 })
   last_digits: string;
 
+  /**
+   * Credit card capacity in COP
+   * @example '1000000.00'
+   */
   @Column({ type: 'decimal', precision: 15, scale: 2 })
   capacity: number;
 
+  /**
+   * Credit card available amount in COP
+   * @example '500000.00'
+   */
   @Column({ type: 'decimal', precision: 15, scale: 2 })
   available: number;
 
+  /**
+   * Credit card interest rate in %
+   * @example '1.0097'
+   */
   @Column({ type: 'decimal', precision: 5, scale: 4 })
   interestRate: number;
 }
